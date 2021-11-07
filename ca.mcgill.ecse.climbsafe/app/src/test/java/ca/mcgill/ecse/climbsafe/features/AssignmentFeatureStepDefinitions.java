f646078e412a74eeb12ea54ee3c849a3ae60f398
seb8stien1 <90061666+seb8stien1@users.noreply.github.com>
2021-11-07 18:33:49 -0500

    AssignmentFeatureStepDefinitions.java
    added file to main branch

---
+++ ca.mcgill.ecse.climbsafe/app/src/test/java/ca/mcgill/ecse/climbsafe/features/AssignmentFeatureStepDefinitions.java
@@ -0,0 +1,213 @@
+package ca.mcgill.ecse.climbsafe.features;
+
+import io.cucumber.java.en.Given;
+import io.cucumber.java.en.Then;
+import io.cucumber.java.en.When;
+
+public class AssignmentFeatureStepDefinitions {
+  @Given("the following ClimbSafe system exists:")
+  public void the_following_climb_safe_system_exists(io.cucumber.datatable.DataTable dataTable) {
+    // Write code here that turns the phrase above into concrete actions
+    // For automatic transformation, change DataTable to one of
+    // E, List<E>, List<List<E>>, List<Map<K,V>>, Map<K,V> or
+    // Map<K, List<V>>. E,K,V must be a String, Integer, Float,
+    // Double, Byte, Short, Long, BigInteger or BigDecimal.
+    //
+    // For other transformations you can register a DataTableType.
+    throw new io.cucumber.java.PendingException();
+  }
+
+  @Given("the following pieces of equipment exist in the system:")
+  public void the_following_pieces_of_equipment_exist_in_the_system(
+      io.cucumber.datatable.DataTable dataTable) {
+    // Write code here that turns the phrase above into concrete actions
+    // For automatic transformation, change DataTable to one of
+    // E, List<E>, List<List<E>>, List<Map<K,V>>, Map<K,V> or
+    // Map<K, List<V>>. E,K,V must be a String, Integer, Float,
+    // Double, Byte, Short, Long, BigInteger or BigDecimal.
+    //
+    // For other transformations you can register a DataTableType.
+    throw new io.cucumber.java.PendingException();
+  }
+
+  @Given("the following equipment bundles exist in the system:")
+  public void the_following_equipment_bundles_exist_in_the_system(
+      io.cucumber.datatable.DataTable dataTable) {
+    // Write code here that turns the phrase above into concrete actions
+    // For automatic transformation, change DataTable to one of
+    // E, List<E>, List<List<E>>, List<Map<K,V>>, Map<K,V> or
+    // Map<K, List<V>>. E,K,V must be a String, Integer, Float,
+    // Double, Byte, Short, Long, BigInteger or BigDecimal.
+    //
+    // For other transformations you can register a DataTableType.
+    throw new io.cucumber.java.PendingException();
+  }
+
+  @Given("the following guides exist in the system:")
+  public void the_following_guides_exist_in_the_system(io.cucumber.datatable.DataTable dataTable) {
+    // Write code here that turns the phrase above into concrete actions
+    // For automatic transformation, change DataTable to one of
+    // E, List<E>, List<List<E>>, List<Map<K,V>>, Map<K,V> or
+    // Map<K, List<V>>. E,K,V must be a String, Integer, Float,
+    // Double, Byte, Short, Long, BigInteger or BigDecimal.
+    //
+    // For other transformations you can register a DataTableType.
+    throw new io.cucumber.java.PendingException();
+  }
+
+  @Given("the following members exist in the system:")
+  public void the_following_members_exist_in_the_system(io.cucumber.datatable.DataTable dataTable) {
+    // Write code here that turns the phrase above into concrete actions
+    // For automatic transformation, change DataTable to one of
+    // E, List<E>, List<List<E>>, List<Map<K,V>>, Map<K,V> or
+    // Map<K, List<V>>. E,K,V must be a String, Integer, Float,
+    // Double, Byte, Short, Long, BigInteger or BigDecimal.
+    //
+    // For other transformations you can register a DataTableType.
+    throw new io.cucumber.java.PendingException();
+  }
+
+  @When("the administrator attempts to initiate the assignment process")
+  public void the_administrator_attempts_to_initiate_the_assignment_process() {
+    // Write code here that turns the phrase above into concrete actions
+    throw new io.cucumber.java.PendingException();
+  }
+
+  @Then("the following assignments shall exist in the system:")
+  public void the_following_assignments_shall_exist_in_the_system(
+      io.cucumber.datatable.DataTable dataTable) {
+    // Write code here that turns the phrase above into concrete actions
+    // For automatic transformation, change DataTable to one of
+    // E, List<E>, List<List<E>>, List<Map<K,V>>, Map<K,V> or
+    // Map<K, List<V>>. E,K,V must be a String, Integer, Float,
+    // Double, Byte, Short, Long, BigInteger or BigDecimal.
+    //
+    // For other transformations you can register a DataTableType.
+    throw new io.cucumber.java.PendingException();
+  }
+
+  @Then("the assignment for {string} shall be marked as {string}")
+  public void the_assignment_for_shall_be_marked_as(String string, String string2) {
+    // Write code here that turns the phrase above into concrete actions
+    throw new io.cucumber.java.PendingException();
+  }
+
+  @Then("the number of assignments in the system shall be {string}")
+  public void the_number_of_assignments_in_the_system_shall_be(String string) {
+    // Write code here that turns the phrase above into concrete actions
+    throw new io.cucumber.java.PendingException();
+  }
+
+  @Then("the system shall raise the error {string}")
+  public void the_system_shall_raise_the_error(String string) {
+    // Write code here that turns the phrase above into concrete actions
+    throw new io.cucumber.java.PendingException();
+  }
+
+  @Given("the following assignments exist in the system:")
+  public void the_following_assignments_exist_in_the_system(
+      io.cucumber.datatable.DataTable dataTable) {
+    // Write code here that turns the phrase above into concrete actions
+    // For automatic transformation, change DataTable to one of
+    // E, List<E>, List<List<E>>, List<Map<K,V>>, Map<K,V> or
+    // Map<K, List<V>>. E,K,V must be a String, Integer, Float,
+    // Double, Byte, Short, Long, BigInteger or BigDecimal.
+    //
+    // For other transformations you can register a DataTableType.
+    throw new io.cucumber.java.PendingException();
+  }
+
+  @When("the administrator attempts to confirm payment for {string} using authorization code {string}")
+  public void the_administrator_attempts_to_confirm_payment_for_using_authorization_code(
+      String string, String string2) {
+    // Write code here that turns the phrase above into concrete actions
+    throw new io.cucumber.java.PendingException();
+  }
+
+  @Then("the assignment for {string} shall record the authorization code {string}")
+  public void the_assignment_for_shall_record_the_authorization_code(String string,
+      String string2) {
+    // Write code here that turns the phrase above into concrete actions
+    throw new io.cucumber.java.PendingException();
+  }
+
+  @Then("the member account with the email {string} does not exist")
+  public void the_member_account_with_the_email_does_not_exist(String string) {
+    // Write code here that turns the phrase above into concrete actions
+    throw new io.cucumber.java.PendingException();
+  }
+
+  @Then("there are {string} members in the system")
+  public void there_are_members_in_the_system(String string) {
+    // Write code here that turns the phrase above into concrete actions
+    throw new io.cucumber.java.PendingException();
+  }
+
+  @Then("the error {string} shall be raised")
+  public void the_error_shall_be_raised(String string) {
+    // Write code here that turns the phrase above into concrete actions
+    throw new io.cucumber.java.PendingException();
+  }
+
+  @When("the administrator attempts to cancel the trip for {string}")
+  public void the_administrator_attempts_to_cancel_the_trip_for(String string) {
+    // Write code here that turns the phrase above into concrete actions
+    throw new io.cucumber.java.PendingException();
+  }
+
+  @Given("the member with {string} has paid for their trip")
+  public void the_member_with_has_paid_for_their_trip(String string) {
+    // Write code here that turns the phrase above into concrete actions
+    throw new io.cucumber.java.PendingException();
+  }
+
+  @Then("the member with email address {string} shall receive a refund of {string} percent")
+  public void the_member_with_email_address_shall_receive_a_refund_of_percent(String string,
+      String string2) {
+    // Write code here that turns the phrase above into concrete actions
+    throw new io.cucumber.java.PendingException();
+  }
+
+  @Given("the member with {string} has started their trip")
+  public void the_member_with_has_started_their_trip(String string) {
+    // Write code here that turns the phrase above into concrete actions
+    throw new io.cucumber.java.PendingException();
+  }
+
+  @When("the administrator attempts to finish the trip for the member with email {string}")
+  public void the_administrator_attempts_to_finish_the_trip_for_the_member_with_email(
+      String string) {
+    // Write code here that turns the phrase above into concrete actions
+    throw new io.cucumber.java.PendingException();
+  }
+
+  @Given("the member with {string} is banned")
+  public void the_member_with_is_banned(String string) {
+    // Write code here that turns the phrase above into concrete actions
+    throw new io.cucumber.java.PendingException();
+  }
+
+  @Then("the member with email {string} shall be {string}")
+  public void the_member_with_email_shall_be(String string, String string2) {
+    // Write code here that turns the phrase above into concrete actions
+    throw new io.cucumber.java.PendingException();
+  }
+
+  @When("the administrator attempts to start the trips for week {string}")
+  public void the_administrator_attempts_to_start_the_trips_for_week(String string) {
+    // Write code here that turns the phrase above into concrete actions
+    throw new io.cucumber.java.PendingException();
+  }
+
+  @Given("the member with {string} has cancelled their trip")
+  public void the_member_with_has_cancelled_their_trip(String string) {
+    // Write code here that turns the phrase above into concrete actions
+    throw new io.cucumber.java.PendingException();
+  }
+
+  @Given("the member with {string} has finished their trip")
+  public void the_member_with_has_finished_their_trip(String string) {
+    // Write code here that turns the phrase above into concrete actions
+    throw new io.cucumber.java.PendingException();
+  }
+}
