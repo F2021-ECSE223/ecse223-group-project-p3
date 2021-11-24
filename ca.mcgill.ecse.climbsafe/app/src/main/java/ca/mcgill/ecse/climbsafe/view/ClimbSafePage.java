952348bb45de84997d9b509185150edb8235161d
seb8stien1 <90061666+seb8stien1@users.noreply.github.com>
2021-11-23 22:57:50 -0500

    Update ClimbSafePage.java

--- ca.mcgill.ecse.climbsafe/app/src/main/java/ca/mcgill/ecse/climbsafe/view/ClimbSafePage.java
+++ ca.mcgill.ecse.climbsafe/app/src/main/java/ca/mcgill/ecse/climbsafe/view/ClimbSafePage.java
@@ -1,1 +1,25 @@
+package ca.mcgill.ecse.climbsafe.view;
 
+import java.awt.Color;
+import java.awt.Font;
+import java.awt.event.ActionEvent;
+import java.util.Map;
+import java.util.Properties;
+
+import javax.swing.GroupLayout;
+import javax.swing.JButton;
+import javax.swing.JComboBox;
+import javax.swing.JFrame;
+import javax.swing.JLabel;
+import javax.swing.JSeparator;
+import javax.swing.JTextField;
+import javax.swing.SwingConstants;
+import javax.swing.WindowConstants;
+
+
+
+public class ClimbSafePage {
+    JFrame mainFrame = new JFrame();
+
+
+}
