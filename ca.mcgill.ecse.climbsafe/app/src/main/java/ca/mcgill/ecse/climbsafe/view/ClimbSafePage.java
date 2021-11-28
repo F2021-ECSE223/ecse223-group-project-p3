0c86eb1e757480d2b3b87d94cd2e99576098e97d
seb8stien1 <90061666+seb8stien1@users.noreply.github.com>
2021-11-28 02:51:23 -0500

    Added base code to ClimbSafePage.java
    Added base code to allow for multiple frames. Instead of adding elements to the Frame, add them to the proper JPanel in the function for each page.

--- ca.mcgill.ecse.climbsafe/app/src/main/java/ca/mcgill/ecse/climbsafe/view/ClimbSafePage.java
+++ ca.mcgill.ecse.climbsafe/app/src/main/java/ca/mcgill/ecse/climbsafe/view/ClimbSafePage.java
@@ -24,21 +24,62 @@ public class ClimbSafePage {
     
 
     public static void start(){
-        //UIManager.put("swing.boldMetal", Boolean.FALSE); //for some reason this is key
         mainFrame = new JFrame("ClimbSafe");
         mainFrame.setPreferredSize(new Dimension(1920, 1080));
         //mainFrame.setIconImage();
         mainFrame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
         tabbedPane = new JTabbedPane();
-        addAdminCard();
+        addNMCCard();
+        addMemberCard();
+        addGuideCard();
+        addEquipmentCard();
+        addBundleCard();
+        addAssignmentCard();
+        addPayCard();
+        AddTripCard();
         
         mainFrame.getContentPane().add(tabbedPane, BorderLayout.CENTER);
         mainFrame.pack();
         mainFrame.setVisible(true);
     }
-    public static void addAdminCard(){
+    public static void addNMCCard(){
         JPanel card1 = new JPanel() {};
         
-        tabbedPane.addTab("Admin Page", card1);
+        tabbedPane.addTab("Setup NMC", card1);
+    }
+    public static void addMemberCard(){
+        JPanel card2 = new JPanel() {};
+        
+        tabbedPane.addTab("Register, Update and Delete Member", card2);
+    }
+    public static void addGuideCard(){
+        JPanel card3 = new JPanel() {};
+        
+        tabbedPane.addTab("Register, Update and Delete Guide", card3);
+    }
+    public static void addEquipmentCard(){
+        JPanel card4 = new JPanel() {};
+        
+        tabbedPane.addTab("Add, Update and Delete Equipment", card4);
+    }
+    public static void addBundleCard(){
+        JPanel card5 = new JPanel() {};
+        
+        tabbedPane.addTab("Add, Update and Delete Equipment Bundle", card5);
+    }
+    public static void addAssignmentCard(){
+        JPanel card6 = new JPanel() {};
+        
+        tabbedPane.addTab("Initiate and View Assignments", card6);
+    }
+    public static void addPayCard(){
+        JPanel card7 = new JPanel() {};
+        
+        tabbedPane.addTab("Pay for Member's Trip", card7);
+    }
+    public static void addTripCard(){
+        JPanel card8 = new JPanel() {};
+        
+        tabbedPane.addTab("Start trips, finish a trip and cancel a trip", card8);
     }
 }
